ecabd07903de66a65866cd59d428408084af47c0
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-10-17 11:05:29 -0300

    Commit inicial- Configurado ambiente para o teste

--- MicrosoftTest/Program.cs
+++ MicrosoftTest/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MicrosoftTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+
+        internal static void Solution1()
+        {
+
+        }
+        internal static void Solution2()
+        {
+
+        }
+
+        internal static void Solution3()
+        {
+
+        }
+    }
+}
