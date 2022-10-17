4a4bc8d6958f07a8e13c5754b213de5d624d7e08
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-10-17 17:20:47 -0300

    Add: Teste unitario do EqualCars

---
+++ TestProject/EqualCars.cs
@@ -0,0 +1,27 @@
+﻿using MicrosoftTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject
+{
+    public class EqualCars
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            var cars = new string[] { "100", "110", "010", "011", "100" };
+            var expectedResult = new int[] { 2, 3, 2, 1, 2 };
+
+            //Act
+            var result = Program.EqualCars(cars);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
