0dcad30135fbf7e64c54dbfa87b2d5c9f5f7235c
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-10-17 17:04:45 -0300

    Add: Soluções entregues no teste tecnico

--- MicrosoftTest/Program.cs
+++ MicrosoftTest/Program.cs
@@ -10,13 +10,30 @@ namespace MicrosoftTest
         {
         }
 
-        internal static int Solution1(int N)
+        #region DigitsSum
+        internal static int DigitsSum(int N)
         {
             int sum = GetSum(N) * 2;
-            for (int i = N + 1; i < int.MaxValue; i++)
+            for (int i = N; i < int.MaxValue; GetNextCompare(ref i))
                 if (GetSum(i) == sum)
                     return i;
-            return 0;
+            return -1;
+        }
+
+        internal static void GetNextCompare(ref int N)
+        {
+            var digits = N.ToString().Select(c => c - '0').ToList();
+            if (digits.All(d => d == 9))
+            {
+                N += (int)Math.Pow(10, digits.Count);
+                return;
+            }
+            var i = digits.LastIndexOf(digits.Last(a => a != 9));
+            digits[i] = digits[i] + 1;
+            int result = 0;
+            for (int j = 0; j < digits.Count; j++)
+                result = result * 10 + digits[j];
+            N = result;
         }
 
         internal static int GetSum(int N)
@@ -29,26 +46,27 @@ namespace MicrosoftTest
             }
             return sum;
         }
+        #endregion
 
-        internal static int[] Solution2(string[] cars)
+        #region EqualCars
+        internal static int[] EqualCars(string[] cars)
         {
             int[] arrInt = new int[cars.Length];
             for (int i = 0; i < cars.Length; i++)
-            {
-                for (int j = i; j < cars.Length; j++)
-                {
+                for (int j = i + 1; j < cars.Length; j++)
                     if (i != j && IsEquals(cars[i], cars[j]))
                     {
                         arrInt[i]++;
                         arrInt[j]++;
                     }
-                }
-            }
             return arrInt;
         }
 
         internal static bool IsEquals(string a, string b)
         {
+            if (a.Equals(b))
+                return true;
+
             int diff = 0;
             for (int i = 0; i < a.Length; i++)
             {
@@ -59,23 +77,21 @@ namespace MicrosoftTest
             }
             return true;
         }
+        #endregion
 
-        internal static int Solution3(string S)
+        #region EvenWord
+        internal static int EvenWord(string S)
         {
-            string sortedWord = string.Concat(S.OrderBy(c => c));
             int remotions = 0;
-            int countLetter = 1;
-            for (int i = 1; i < sortedWord.Length; i++)
-            {
-                if (sortedWord[i] == sortedWord[i - 1])
-                    countLetter++;
-                else if (countLetter % 2 != 0)
-                {
+            var arrInt = new int[26];
+            for (int i = 0; i < S.Length; i++)
+                arrInt[S[i] - 97]++;
+
+            for (int i = 0; i < 26; i++)
+                if (arrInt[i] % 2 == 1)
                     remotions++;
-                    countLetter = 1;
-                }
-            }
-            return countLetter % 2 != 0 ? ++remotions : remotions;
+            return remotions;
         }
+        #endregion
     }
 }
